da160f826d299f68f67cbffbbe134057a0943553
STRRRKA <dtarzimin@mail.ru>
2026-06-03 11:35:28 +0500

    'test'

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
-  return (
-    <>
-<h1 className="text-3xl  underline">    Hello world!  </h1>
-
-    </>
-  )
+    return (
+        <>
+            <header className="container mx-auto bg-red-500 text-white">
+                <div className="flex justify-between pt-10  ">
+                    <span
+                        className="font-bold text-3xl pl-2 cursor-pointer"
+                        onClick={() => (window.location.href = "")}
+                    >
+                        TailWind
+                    </span>
+                    <div className="space-x-5">
+                        <a href="" className="hover:underline">
+                            About
+                        </a>
+                        <a
+                            href=""
+                            className="bg-gray-500 py-2 px-4 rounded-full hover:bg-gray-700"
+                        >
+                            Contacts
+                        </a>
+                    </div>
+                </div>
+            </header>
+        </>
+    );
 }
 
-export default App
+export default App;
