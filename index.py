34a3e58d2d3b1866547b1baa17058acc4e5f8c23
STRRRKA <dtarzimin@mail.ru>
2026-06-04 13:01:04 +0500

    front wipe

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 from concurrent.futures import ThreadPoolExecutor
 from src.media_tools import MediaTools
-from moviepy import VideoFileClip
+
 
 app = Flask(__name__)
 executor = ThreadPoolExecutor(max_workers=5)
@@ -9,12 +9,8 @@ executor = ThreadPoolExecutor(max_workers=5)
 @app.get("/")
 def index():
     try:
-        file = VideoFileClip('input.mp4')
-        sound = file.audio
-        sound.write_audiofile('audio.mp3')
-        # ffmpeg.input("input.mp4").output('audio.mp3').run()
-        # highlighter = MediaTools()
-        # executor.submit(highlighter.run)
+        highlighter = MediaTools()
+        executor.submit(highlighter.run)
         return f'Done!'
     except Exception as e:
         return str(e)
--- src/media_tools.py
+++ src/media_tools.py
@@ -1,4 +1,4 @@
-import ffmpeg
+from moviepy import VideoFileClip
 
 class MediaTools:
     def __init__(self, video='input.mp4'):
@@ -6,9 +6,10 @@ class MediaTools:
         self.audio_path = 'audio.mp3'
 
     def extract_audio(self):
-        ffmpeg.input(self.video_path).output(self.audio_path).run()
+        file = VideoFileClip('input.mp4')
+        sound = file.audio
+        sound.write_audiofile('audio.mp3')
 
     def run(self):
         print('run')
         # self.extract_audio()
-        ffmpeg.input(self.video_path).output(self.audio_path).run() 
